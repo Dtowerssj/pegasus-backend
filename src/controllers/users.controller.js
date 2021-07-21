f3579c1dbfb869bb53faacaabb28d352c20d33c3
Leonardo Chourio <leoch1000@gmail.com>
2021-07-21 14:23:46 -0400

    cambios realizados

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,65 +1,94 @@
-
-const conn = require('../utils/db');
-const queries = require('../utils/queries')
+const conn = require("../utils/db");
+const queries = require("../utils/queries");
 //import { genSaltSync, hashSync } from 'bcryptjs';
 
-
 const getUsuarios = async (req, res) => {
-    console.log("estas en get users")
-    const client = await conn.connect();
-    try {
-        const response = await client.query(queries.GET_USERS);
-        return res.status(200).json(response.rows);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json('Internal Server error');
-    }
-} 
+  console.log("estas en get users");
+  const client = await conn.connect();
+  try {
+    const response = await client.query(queries.GET_USERS);
+    return res.status(200).json(response.rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Internal Server error");
+  }
+};
 
-/*
-export const getUsuariobyId = async (req:Request, res: Response): Promise<Response> => {
+export const getUsuariobyId = async (req, res) => {
+  try {
     const id = parseInt(req.params.id);
-    const response: QueryResult = await conn.query(queries.GET_USER_BY_ID, [id]);
-    return res.json(response.rows);
-}
+    const response = await conn.query(queries.GET_USER_BY_ID, [id]);
+    return res.status(200).json(response.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+};
 
- export const createUsuario = async (req:Request, res: Response): Promise<Response> => {
-    
-    const { nombre, usuario, correo, clave } = req.body;
-    
-    try {
-        const salt = genSaltSync(10);
-        const hashedPassword = hashSync(clave, salt);
-        const response: QueryResult = await conn.query(queries.INSERT_USER, [nombre, usuario.toLowerCase(), correo, hashedPassword ]);
-        return res.json({
-        message: 'Usuario creado',
-        body: {
-            Trabajador: {
-                nombre, usuario, correo, clave
-            }
-        }
-    })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json('Usuario no creado');
-    }
-    
-}
+export const createUsuario = async (req, res) => {
+  const { nombre, usuario, correo, clave } = req.body;
 
-export const updateUsuario = async (req:Request, res: Response): Promise<Response> => {
+  try {
+    const salt = genSaltSync(10);
+    const hashedPassword = hashSync(clave, salt);
+    const response = await conn.query(queries.INSERT_USER, [
+      nombre,
+      usuario.toLowerCase(),
+      correo,
+      hashedPassword,
+    ]);
+    return res.status(200).json({
+      message: "Usuario creado",
+      body: {
+        Trabajador: {
+          nombre,
+          usuario,
+          correo,
+          clave,
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Usuario no creado");
+  }
+};
+
+export const updateUsuario = async (req, res) => {
+  try {
     const id = parseInt(req.params.id);
-    const {usuario, clave, salario } = req.body;
-    const response: QueryResult = await conn.query(queries.UPDATE_USER, [ usuario, clave, salario, id]);
-    return res.json(`Usuario ${id} actualizado satisfactoriamente`);
-}
+    const { usuario, clave, salario } = req.body;
+    const response = await conn.query(queries.UPDATE_USER, [
+      usuario,
+      clave,
+      salario,
+      id,
+    ]);
+    return res.status(200).json(`Usuario ${id} actualizado satisfactoriamente`);
+  } catch (erorr) {
+    console.log(error);
+    res.status(500);
+  }
+};
 
-export const deleteUsuario = async (req:Request, res: Response): Promise<Response> => {
+export const deleteUsuario = async (req, res) => {
+  try {
     const id = parseInt(req.params.id);
-    const response: QueryResult = await conn.query(queries.DELETE_USER, [id]); 
-    return res.json(`Usuario ${id} eliminado satisfactoriamente`);
-} 
-*/
+    const response = await conn.query(queries.DELETE_USER, [id]);
+    return res.status(200).json(`Usuario ${id} eliminado satisfactoriamente`);
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+};
 
 module.exports = {
-    getUsuarios
-}
+  getUsuarios,
+  getUsuariobyId,
+
+  createUsuario,
+
+  updateUsuario,
+
+  deleteUsuario,
+};
