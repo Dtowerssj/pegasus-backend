42aa10d3d45c13cb943ea88520e2780eb23d6a72
Diego Torres <dtorres@binwus.com>
2021-07-21 19:27:01 -0400

    hola

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,17 +3,18 @@ const router = Router();
 
 const {
   getUsuarios,
-  getLogin,
   createUsuario,
   updateUsuario,
   deleteUsuario,
   getUsuariobyId,
-} = require("../controllers/users.controller", "../controllers/auth.controller");
+} = require("../controllers/users.controller");
+
+const { getLogin } = require("../controllers/auth.controller")
 
 router.get("/usuarios", getUsuarios);
 router.get("/:id", getUsuariobyId);
 router.post("/login", getLogin);
-router.post("/", createUsuario);
+router.post("/usuarios", createUsuario);
 router.put("/:id", updateUsuario);
 router.delete("/:id", deleteUsuario);
 
