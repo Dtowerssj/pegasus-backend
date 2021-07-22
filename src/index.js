66be2c401a7c94ce14fb15d6bb7c130c46a918f4
Diego Torres <dtorres@binwus.com>
2021-07-21 21:01:51 -0400

    hola

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const server = require('./server');
 
 PORT = process.env.PORT || 4000;
+DB = process.env.DB_URI;
 
 server.listen(PORT, () => {
     console.log('Server in port: ' + PORT);
+    console.log('DBURL: '+DB )
 });
