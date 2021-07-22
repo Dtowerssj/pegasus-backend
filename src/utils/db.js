7d5b04c6b218836bf8191087b3c0e7f6390d49cf
Diego Torres <dtorres@binwus.com>
2021-07-21 20:41:02 -0400

    hola

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const conn = new Pool({
-  connectionString: process.env.DB_URI
+  connectionString: process.env.DB_URI,
+  max: 20,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 module.exports = conn;
