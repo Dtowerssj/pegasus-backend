0978fe08c8bf5e792f01404227bf1ded0d56d02c
Diego Torres <dtorres@binwus.com>
2021-07-24 11:00:02 -0400

    make it better

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const conn = new Pool({
-  connectionString: 'postgres://gklgboomowxamk:41ae792072f7c6365a319fd4ec73de17e0536b341f502d21b115016001646ee6@ec2-3-233-100-43.compute-1.amazonaws.com:5432/d6p3df57rjsrmh',
+  connectionString: 'postgres://ispvhhxgudkqyb:eb556ba54f30012d2d379fa8671c0a61059684207d7ace3afb4ac089824284ef@ec2-52-86-2-228.compute-1.amazonaws.com:5432/da8nucu6j9f6ud',
   max: 20,
   ssl: {
     rejectUnauthorized: false
